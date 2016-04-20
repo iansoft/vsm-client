9ef72f46adbc6119a5255f25a9e2156f69da9a61
anziloveanzi@hotmail.com <anziloveanzi@hotmail.com>
2016-04-20 08:36:26 +0800

    edit the manage_rbd template

--- vsm_client/manage_rbd/views.py
+++ vsm_client/manage_rbd/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from utils.menu import set_menu,set_breadcrumb
 
 def index(request):
-    menu_list = set_menu("Integrate","Hadoop")
+    menu_list = set_menu("Cluster Management","RBD")
     function_item = {"name":"", "url":""}
-    breadcrumb_list = set_breadcrumb("Integrate","",function_item)
+    breadcrumb_list = set_breadcrumb("Cluster Management","",function_item)
     return render(request, 
-                "integrate_hadoop/index.html",
+                "manage_rbd/index.html",
                 {"menu_list":menu_list, "breadcrumb_list":breadcrumb_list})
