92cd772de9329c5df3038ce0db9e73dac90098e7
anziloveanzi@hotmail.com <anziloveanzi@hotmail.com>
2016-04-21 10:27:21 +0800

    add the device creat/import page

--- vsm_client/manage_device/urls.py
+++ vsm_client/manage_device/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from manage_device import views as device_views
 urlpatterns = [
     url(r'^$', device_views.index,name="index"),
-    #url(r'^add/$', device_views.add_view,name="add"),
+    url(r'^add/$', device_views.add_view,name="add"),
+    url(r'^import/$', device_views.import_view,name="import"),
 ]
--- vsm_client/manage_device/views.py
+++ vsm_client/manage_device/views.py
@@ -9,6 +9,8 @@ def index(request):
             "server":"server_"+str(i),
             "storage_class":"class",
             "zone":"zone1",
+            "weight":2,
+            "capacity":"12.0 GB/24.0 GB(50%)",
             "status":"Active",
         }
         device_list.append(device);
@@ -19,3 +21,17 @@ def index(request):
     return render(request, 
                 "manage_device/index.html",
                 {"menu_list":menu_list, "breadcrumb_list":breadcrumb_list, "device_list":device_list})
+
+
+def add_view(request):
+    menu_list = set_menu("Cluster Management","Device")
+    function_item = {"name":"Add Device", "url":"/device/add/"}
+    breadcrumb_list = set_breadcrumb("Cluster Management","Device",function_item)
+    return render(request, 'manage_device/add.html', {"menu_list":menu_list, "breadcrumb_list":breadcrumb_list})
+
+
+def import_view(request):
+    menu_list = set_menu("Cluster Management","Device")
+    function_item = {"name":"Import Device", "url":"/device/import/"}
+    breadcrumb_list = set_breadcrumb("Cluster Management","Device",function_item)
+    return render(request, 'manage_device/import.html', {"menu_list":menu_list, "breadcrumb_list":breadcrumb_list})
